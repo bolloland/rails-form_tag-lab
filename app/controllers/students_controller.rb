e14ebdf8f67622463d2cdf92bf2ce402bf3aec7c
Ryan Bollettino <ryan.bollettino@gmail.com>
2021-03-22 23:21:31 -0500

    commit tree 79c23eff9ea8b1a1f13d72bbffe60ced2c5b574f

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,7 +9,8 @@ class StudentsController < ApplicationController
   end
 
   def create
-    @student = Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+   byebug
     redirect_to new_student_path
   end
 
